72570a3a29effd71e70fb167f141d65741204415
dani <daninbt2@gmail.com>
2026-08-24 16:43:40 +0430

    practice session4

---
+++ alaki/practice session4.py
@@ -0,0 +1,16 @@
+# 1)
+a = int(input("لطفا یک عدد وارد کنید:"))
+b = 0
+for i in range(1,a):
+    if a % i ==0
+    b = b + i
+    if b == a
+    print(f"{a}یک عدد کامل است")
+else:
+    print(f"{a}یک عدد کامل نیست")
+#2)
+a = ['red','blue','yellow','green']
+b=[]
+for i in range(len(a)-1,-1,-1):
+    b.append(a[i])
+    print(b)
